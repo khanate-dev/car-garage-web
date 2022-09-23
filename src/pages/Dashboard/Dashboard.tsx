3f35f9d764e41ec691ff7620bb937d0c91770829
SuperKXT <superkxt@outlook.com>
2022-09-23 17:18:20 +0500

    Revamp Dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,8 @@
-import Button from 'components/Button';
+import Button, { buttonSizes, buttonVariants } from 'components/Button';
 import { useDarkMode } from 'contexts/dark-mode';
 
 import { ReactComponent as Logo } from 'logo.svg';
+import { themeColors } from 'types/general';
 
 import styles from './Dashboard.module.scss';
 
@@ -12,20 +13,9 @@ const Dashboard = () => {
 	return (
 		<main>
 
-			<Logo className={styles['app-logo']} />
-
-			<p>
-				Edit <code>src/App.tsx</code> and save to reload.
-			</p>
-
-			<a
-				className="App-link"
-				href="https://reactjs.org"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Learn React
-			</a>
+			<h1>
+				Car Garage Web App
+			</h1>
 
 			<Button
 				onClick={(event) => {
@@ -34,7 +24,43 @@ const Dashboard = () => {
 				}}
 				text={`Switch To: ${isDarkMode ? 'Light' : 'Dark'}`}
 				color='primary'
+				icon={Logo}
 			/>
+
+			<input type='checkbox' />
+			<div className={styles['flex']}>
+				<input name='radio' type='radio' />
+				<input name='radio' type='radio' />
+			</div>
+			<input type='range' />
+			<progress></progress>
+
+			<div className={styles['flex']}>
+				{buttonSizes.map(size =>
+					<Button
+						key={size}
+						color='primary'
+						variant='fill'
+						size={size}
+						text='Click Me!'
+						icon={Logo}
+					/>
+				)}
+			</div>
+
+			<div className={styles['button-showcase']}>
+				{buttonVariants.map(variant =>
+					themeColors.map(color =>
+						<Button
+							key={`${variant}-${color}`}
+							color={color}
+							variant={variant}
+							text='Click Me!'
+							icon={Logo}
+						/>
+					)
+				)}
+			</div>
 
 		</main>
 	);
