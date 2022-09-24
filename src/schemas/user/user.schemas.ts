61bbc715d98a75bebd4efae2cadfbdf8b524c9e9
SuperKXT <saadkhankxt@hotmail.com>
2022-09-24 20:24:25 +0500

    Add user schemas

---
+++ src/schemas/user/user.schemas.ts
@@ -0,0 +1,45 @@
+import z from 'zod';
+import { PHONE_REGEX } from 'config';
+
+import { getModelSchema } from 'helpers/schema';
+
+export const userRoles = [
+	'user',
+	'admin',
+	'guest',
+] as const;
+
+export type UserRole = typeof userRoles[number];
+
+export const {
+	sansMetaModelSchema: userSansMetaModelSchema,
+	modelSchema: userModelSchema,
+} = getModelSchema({
+	email: z.string().email(),
+	phoneNumber: z.string().regex(PHONE_REGEX),
+	name: z.string(),
+	password: z.string(),
+	role: z.enum(userRoles),
+});
+
+export type UserSansMeta = z.infer<typeof userSansMetaModelSchema>;
+
+export type User = z.infer<typeof userModelSchema>;
+
+export const userSansPasswordModelSchema = userModelSchema.omit({
+	password: true,
+});
+
+export type UserSansPassword = z.infer<typeof userSansPasswordModelSchema>;
+
+export const defaultUser: User = {
+	_id: '1234',
+	name: 'john.doe',
+	email: 'john@doe.com',
+	role: 'admin',
+	phoneNumber: '1234',
+	password: 'password',
+	createdAt: new Date(),
+	updatedAt: new Date(),
+	__v: 1,
+};
