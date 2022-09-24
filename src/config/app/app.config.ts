4c53901ca7f8345f60e1c6b4edd10733bf666d15
SuperKXT <saadkhankxt@hotmail.com>
2022-09-24 18:51:44 +0500

    Cleanup unused config and add constants

--- src/config/app/app.config.ts
+++ src/config/app/app.config.ts
@@ -1,6 +1,5 @@
 const {
 	REACT_APP_BACKEND_API_PATH,
-	REACT_APP_NATS_SERVER,
 	NODE_ENV,
 } = process.env;
 
@@ -30,23 +29,6 @@ const backendApiEndpoint: string = (
 	?? 'http://localhost:5000'
 );
 
-/** the base url path for the NATS WebSocket server */
-const natsServer: string = (
-	REACT_APP_NATS_SERVER
-	?? 'ws://localhost:4222'
-);
-
-const isFetchMockedConfig: Record<Environment, boolean> = {
-	development: true,
-	test: true,
-	production: false,
-};
-/** should the app use dummy data? used for demos of the frontend */
-const isFetchMocked: boolean = isFetchMockedConfig[appEnvironment];
-
-/** should notifications be turned off? */
-const disableNotifications = true;
-
 const disableAuthConfig: Record<Environment, boolean> = {
 	development: true,
 	test: true,
@@ -58,8 +40,5 @@ const disableAuth = disableAuthConfig[appEnvironment];
 export {
 	appEnvironment,
 	backendApiEndpoint,
-	natsServer,
-	isFetchMocked,
-	disableNotifications,
 	disableAuth,
 };
