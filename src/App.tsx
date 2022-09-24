00ac2cf7a19cdb394a371b58531aacea7524c70f
SuperKXT <saadkhankxt@hotmail.com>
2022-09-24 15:18:49 +0500

    Setup react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import Providers from 'components/app/Providers';
 
 import Dashboard from 'pages/Dashboard';
+import Error from 'pages/Error';
+import {
+	createBrowserRouter,
+	RouterProvider,
+	Route,
+	json,
+} from 'react-router-dom';
 
 import 'theme/theme.scss';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Dashboard />,
+		errorElement: <Error />,
+	},
+]);
 
 function App() {
 
 	return (
 		<Providers>
-			<Dashboard />
+			<RouterProvider
+				router={router}
+			/>
 		</Providers>
 	);
 }
