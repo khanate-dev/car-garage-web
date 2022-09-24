e0a7a271fbd837cd27306add9cfb011a7fc719af
SuperKXT <saadkhankxt@hotmail.com>
2022-09-24 15:18:40 +0500

    Add Error page

---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,35 @@
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+
+import styles from './Error.module.scss';
+
+const Error = () => {
+
+	const error = useRouteError();
+
+	return (
+		<div className={styles['container']}>
+			<h1
+				className={styles['heading']}
+			>
+				Oops!
+			</h1>
+
+			<p>Sorry, An Unexpected Error Has Occurred.</p>
+
+			{isRouteErrorResponse(error) &&
+				<>
+					<h3>{error.status} - {error.statusText}</h3>
+					{error.data?.message &&
+						<code className={styles['error-message']}>
+							{error.data.message}
+						</code>
+					}
+				</>
+			}
+
+		</div>
+	);
+
+};
+
+export default Error;
