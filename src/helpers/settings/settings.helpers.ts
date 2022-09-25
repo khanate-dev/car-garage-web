f4bc86689d3b196ee9976f43e393603152aceb4c
SuperKXT <superkxt@outlook.com>
2022-09-25 22:48:38 +0500

    Use zod schemas in setting helpers

--- src/helpers/settings/settings.helpers.ts
+++ src/helpers/settings/settings.helpers.ts
@@ -1,6 +1,16 @@
-import { isValidUser } from 'helpers/type';
+import { z, ZodSchema } from 'zod';
+
+import { userSansPasswordModelSchema } from 'schemas/user';
+import { jwtSchema } from 'schemas/session';
 
 import { Settings } from 'types/general';
+
+const schemaMapping: Record<keyof Settings, ZodSchema> = {
+	isDarkMode: z.boolean(),
+	user: userSansPasswordModelSchema,
+	accessToken: jwtSchema,
+	refreshToken: jwtSchema,
+};
 
 const getSetting = <Key extends keyof Settings>(
 	key: Key
@@ -10,20 +20,13 @@ const getSetting = <Key extends keyof Settings>(
 		const result = localStorage.getItem(key);
 		if (!result) return null;
 
-		const parsed = JSON.parse(result);
-		const isInvalid = (
-			(key === 'isDarkMode' && typeof parsed !== 'boolean')
-			|| (key === 'user' && !isValidUser(parsed))
-			|| (['accessToken', 'refreshToken'].includes(key) && typeof parsed !== 'string')
-		);
-		if (isInvalid) {
-			removeSetting(key);
-			return null;
-		}
-		return parsed;
+		const schema = schemaMapping[key];
+		const setting = schema.parse(JSON.parse(result));
+		return setting;
 
 	}
 	catch (error) {
+		removeSetting(key);
 		return null;
 	}
 };
@@ -32,7 +35,9 @@ const setSetting = <Key extends keyof Settings>(
 	key: Key,
 	value: Settings[Key]
 ): void => {
-	localStorage.setItem(key, JSON.stringify(value));
+	const schema = schemaMapping[key];
+	const setting = schema.parse(value);
+	localStorage.setItem(key, JSON.stringify(setting));
 };
 
 const removeSetting = (
