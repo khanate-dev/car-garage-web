e701a0b66ff7ffee692c5c2fd73c27c6ebb9217e
SuperKXT <superkxt@outlook.com>
2022-09-27 14:34:32 +0500

    Add register route

--- src/routes/register/register.tsx
+++ src/routes/register/register.tsx
@@ -1,21 +1,26 @@
-import { FormEventHandler, useReducer } from 'react';
+import { FormEventHandler } from 'react';
 import { LoaderFunction, redirect, useNavigate } from 'react-router-dom';
+
+import useFormError from 'hooks/form-error';
 
 import {
 	loginRequestSchema,
 	loginResponseSchema,
 } from 'schemas/session';
-import { UserSansMeta } from 'schemas/user';
+import { userRoles, UserSansMeta } from 'schemas/user';
 import FormError from 'errors/form';
 
 import { postRequest } from 'helpers/api';
 import { getSetting, setSetting } from 'helpers/settings';
 
 import ThemeSwitch from 'components/ThemeSwitch';
-import Input, { InputProps } from 'components/Input';
 import Button from 'components/Button';
+import FormField from 'components/FormField';
+
+import { FormField as FormFieldType } from 'types/general';
 
 import styles from './register.module.scss';
+import { Link } from 'react-router-dom';
 
 export const registerLoader: LoaderFunction = async () => {
 	const user = getSetting('user');
@@ -23,50 +28,33 @@ export const registerLoader: LoaderFunction = async () => {
 	return;
 };
 
-type ErrorReducerAction = (
-	| { type: 'reset', }
-	| { type: 'remove-field', value: (keyof UserSansMeta) | (keyof UserSansMeta)[], }
-	| { type: 'update', value: FormError<UserSansMeta>, }
-);
-
-const errorReducer = (
-	prev: null | FormError<UserSansMeta>,
-	action: ErrorReducerAction
-): null | FormError<UserSansMeta> => {
-	switch (action.type) {
-		case 'remove-field': {
-			if (!prev) return null;
-			const fieldsToRemove = (
-				Array.isArray(action.value)
-					? action.value
-					: [action.value]
-			);
-			fieldsToRemove.forEach(field =>
-				delete prev.fieldErrors[field]
-			);
-			return new FormError(prev.fieldErrors);
-		}
-		case 'update':
-			return action.value;
-		case 'reset':
-			return null;
-		default:
-			throw new Error('Invalid action type');
-	}
-};
-
-const inputs: InputProps[] = [
+const fields: FormFieldType<UserSansMeta>[] = [
 	{
+		fieldType: 'input',
+		name: 'name',
+		type: 'input',
+		required: true,
+	},
+	{
+		fieldType: 'input',
 		name: 'email',
 		type: 'email',
 		required: true,
 	},
 	{
+		fieldType: 'input',
 		name: 'phoneNumber',
 		type: 'phone',
 		required: true,
 	},
 	{
+		fieldType: 'select',
+		name: 'role',
+		required: true,
+		options: userRoles as any,
+	},
+	{
+		fieldType: 'input',
 		name: 'password',
 		type: 'password',
 		description: 'Must be at least 6 characters',
@@ -74,11 +62,11 @@ const inputs: InputProps[] = [
 	},
 ];
 
-export const Login = () => {
+export const Register = () => {
 
 	const navigate = useNavigate();
 
-	const [error, dispatchError] = useReducer(errorReducer, null);
+	const [error, dispatchError] = useFormError<UserSansMeta>();
 
 	const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
 		try {
@@ -110,9 +98,11 @@ export const Login = () => {
 		<main
 			className={styles['container']}
 		>
+
 			<ThemeSwitch
 				className={styles['theme-switch']}
 			/>
+
 			<form
 				className={styles['form']}
 				onSubmit={handleSubmit}
@@ -121,30 +111,22 @@ export const Login = () => {
 				<h1>Car Garage</h1>
 				<p>Welcome! Register below</p>
 
-				<Input
-					name='email'
-					type='email'
-					error={error?.fieldErrors.email}
-					onErrorReset={() => {
-						dispatchError({
-							type: 'remove-field', value: 'email',
-						});
-					}}
-					required
-				/>
-
-				<Input
-					name='password'
-					type='password'
-					error={error?.fieldErrors.password}
-					onErrorReset={() => {
-						dispatchError({
-							type: 'remove-field', value: 'password',
-						});
-					}}
-					description='Must be at least 6 characters'
-					required
-				/>
+				<div
+					className={styles['fields']}
+				>
+					{fields.map(field =>
+						<FormField
+							key={field.name}
+							field={field}
+							error={error?.fieldErrors[field.name]}
+							onErrorReset={() => {
+								dispatchError({
+									type: 'remove-field', value: field.name,
+								});
+							}}
+						/>
+					)}
+				</div>
 
 				{error?.isGeneral &&
 					<div
@@ -159,7 +141,14 @@ export const Login = () => {
 					type='submit'
 				/>
 
+				<Link
+					to='/login'
+				>
+					Have An Account? Login here
+				</Link>
+
 			</form>
+
 		</main>
 	);
 
