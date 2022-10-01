249de3c5b58ea90c8918cef82fe8e25891d0a029
SuperKXT <superkxt@outlook.com>
2022-10-01 23:31:10 +0500

    Add corner type

--- src/types/general/general.types.ts
+++ src/types/general/general.types.ts
@@ -115,3 +115,11 @@ export const sizes = [
 ] as const;
 
 export type Size = typeof sizes[number];
+
+const corners = [
+	'flat',
+	'rounded',
+	'circular',
+] as const;
+
+export type Corner = typeof corners[number];
