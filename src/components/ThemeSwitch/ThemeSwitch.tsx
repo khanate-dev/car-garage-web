0728c25e4ff73b5cbc92abee4f62a5bdc509e652
SuperKXT <superkxt@outlook.com>
2022-10-01 23:27:24 +0500

    Rewrite ThemeSwitch to use IconButton

--- src/components/ThemeSwitch/ThemeSwitch.tsx
+++ src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -2,6 +2,8 @@ import { useDarkMode } from 'contexts/dark-mode';
 
 import { cx } from 'helpers/class-name';
 
+import IconButton from 'components/IconButton';
+
 import { ReactComponent as LightModeIcon } from 'icons/light-mode.svg';
 import { ReactComponent as DarkModeIcon } from 'icons/dark-mode.svg';
 
@@ -16,25 +18,24 @@ const ThemeSwitch = ({
 	const { isDarkMode, toggleDarkMode } = useDarkMode();
 
 	return (
-		<button
+		<IconButton
 			className={cx(
-				styles['switch'],
 				isDarkMode ? styles['dark'] : styles['light'],
-				className,
-				size
+				className
 			)}
-			onClick={(event) => {
-				toggleDarkMode();
-				event.currentTarget.blur();
-			}}
-		>
-			<LightModeIcon
-				className={styles['light-icon']}
-			/>
-			<DarkModeIcon
-				className={styles['dark-icon']}
-			/>
-		</button>
+			size={size}
+			onClick={toggleDarkMode}
+			icon={
+				<>
+					<LightModeIcon
+						className={styles['light-icon']}
+					/>
+					<DarkModeIcon
+						className={styles['dark-icon']}
+					/>
+				</>
+			}
+		/>
 	);
 
 };
