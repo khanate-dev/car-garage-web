4cdbbd6e9add5e7e913caf6eadb4c7a7ac273664
SuperKXT <superkxt@outlook.com>
2022-10-04 00:29:43 +0500

    Add createModel endpoint

--- src/endpoints/model/model.endpoints.ts
+++ src/endpoints/model/model.endpoints.ts
@@ -1,10 +1,25 @@
 import { z } from 'zod';
 
-import { modelModelSchema } from 'schemas/model';
+import {
+	CreateModelResponse,
+	createModelResponseSchema,
+	createModelSchema,
+	modelModelSchema,
+} from 'schemas/model';
 
-import { getRequest } from 'helpers/api';
+import { getRequest, postRequest } from 'helpers/api';
 
 export const getModels = async () => {
 	const models = await getRequest('model');
 	return z.array(modelModelSchema).parse(models);
+};
+
+export const createModel = async (
+	formData: FormData
+): Promise<CreateModelResponse> => {
+	const json = createModelSchema.parse(
+		Object.fromEntries(formData)
+	);
+	const model = await postRequest('model', json);
+	return createModelResponseSchema.parse(model);
 };
