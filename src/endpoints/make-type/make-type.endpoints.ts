532a72ef4174fe450e82d6c609461772746cceca
SuperKXT <superkxt@outlook.com>
2022-10-03 23:19:48 +0500

    Add createMakeTypes endpoint

--- src/endpoints/make-type/make-type.endpoints.ts
+++ src/endpoints/make-type/make-type.endpoints.ts
@@ -1,10 +1,22 @@
 import { z } from 'zod';
 
-import { makeTypeModelSchema } from 'schemas/make-type';
+import {
+	MakeType,
+	makeTypeModelSchema,
+	makeTypeSansMetaModelSchema,
+} from 'schemas/make-type';
 
-import { getRequest } from 'helpers/api';
+import { getRequest, postRequest } from 'helpers/api';
 
 export const getMakeTypes = async () => {
 	const makeTypes = await getRequest('make-type');
 	return z.array(makeTypeModelSchema).parse(makeTypes);
+};
+
+export const createMakeType = async (
+	formData: FormData
+): Promise<MakeType> => {
+	const body = makeTypeSansMetaModelSchema.parse(Object.fromEntries(formData));
+	const response = await postRequest('make-type', body);
+	return makeTypeModelSchema.parse(response);
 };
