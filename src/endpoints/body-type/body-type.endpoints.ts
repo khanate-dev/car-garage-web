9701bb0da23eaa7ee2ae04b8318adca42f0875af
SuperKXT <superkxt@outlook.com>
2022-10-04 00:29:32 +0500

    Add createBodyType endpoint

--- src/endpoints/body-type/body-type.endpoints.ts
+++ src/endpoints/body-type/body-type.endpoints.ts
@@ -1,10 +1,25 @@
 import { z } from 'zod';
 
-import { bodyTypeModelSchema } from 'schemas/body-type';
+import {
+	bodyTypeModelSchema,
+	CreateBodyTypeResponse,
+	createBodyTypeResponseSchema,
+	createBodyTypeSchema,
+} from 'schemas/body-type';
 
-import { getRequest } from 'helpers/api';
+import { getRequest, postRequest } from 'helpers/api';
 
 export const getBodyTypes = async () => {
 	const bodyTypes = await getRequest('body-type');
 	return z.array(bodyTypeModelSchema).parse(bodyTypes);
+};
+
+export const createBodyType = async (
+	formData: FormData
+): Promise<CreateBodyTypeResponse> => {
+	const json = createBodyTypeSchema.parse(
+		Object.fromEntries(formData)
+	);
+	const bodyType = await postRequest('body-type', json);
+	return createBodyTypeResponseSchema.parse(bodyType);
 };
