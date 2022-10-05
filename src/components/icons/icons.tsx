8157ecd1d47649a2b8776c85799b65590a96dd03
SuperKXT <superkxt@outlook.com>
2022-10-05 19:16:04 +0500

    Extend components/icons

--- src/components/icons/icons.tsx
+++ src/components/icons/icons.tsx
@@ -4,13 +4,32 @@ import {
 	PlugIcon,
 	RocketIcon,
 	TrophyIcon,
+	ZapIcon,
+	ArrowLeftIcon,
+	ArrowRightIcon,
+	PlusIcon,
+	ChevronLeftIcon,
+	SignOutIcon,
+	MoonIcon,
+	SunIcon,
+	ImageIcon,
 } from '@primer/octicons-react';
+
 
 export const overviewIcon = <GraphIcon />;
 export const productsIcon = <MegaphoneIcon />;
 export const bodyTypesIcon = <PlugIcon />;
 export const makeTypesIcon = <RocketIcon />;
 export const modelsIcon = <TrophyIcon />;
+export const submitIcon = <ZapIcon />;
+export const backIcon = <ArrowLeftIcon />;
+export const nextIcon = <ArrowRightIcon />;
+export const addIcon = <PlusIcon />;
+export const minimizeIcon = <ChevronLeftIcon />;
+export const logoutIcon = <SignOutIcon />;
+export const darkModeIcon = <MoonIcon />;
+export const lightModeIcon = <SunIcon />;
+export const imageIcon = <ImageIcon />;
 
 export const icons = {
 	overview: overviewIcon,
@@ -18,4 +37,13 @@ export const icons = {
 	bodyTypes: bodyTypesIcon,
 	makeTypes: makeTypesIcon,
 	models: modelsIcon,
+	submit: submitIcon,
+	back: backIcon,
+	next: nextIcon,
+	add: addIcon,
+	minimize: minimizeIcon,
+	logout: logoutIcon,
+	darkMode: darkModeIcon,
+	lightMode: lightModeIcon,
+	image: imageIcon,
 };
