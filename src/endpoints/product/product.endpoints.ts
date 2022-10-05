e9dffad74f5a72344b83bc6a8a9a8dca73abcfff
SuperKXT <superkxt@outlook.com>
2022-10-05 19:18:35 +0500

    Add image upload to createProduct endpoint

--- src/endpoints/product/product.endpoints.ts
+++ src/endpoints/product/product.endpoints.ts
@@ -5,6 +5,7 @@ import {
 	productModelSchema,
 	createProductSchema,
 } from 'schemas/product';
+import { uploadImage } from 'endpoints/imgur';
 
 import { getRequest, postRequest } from 'helpers/api';
 import { getSetting } from 'helpers/settings';
@@ -17,10 +18,18 @@ export const getProducts = async (): Promise<Product[]> => {
 export const createProduct = async (
 	formData: FormData
 ): Promise<Product> => {
+
+	const image = formData.get('image');
+	if (!(image instanceof File)) {
+		throw new Error('Image must be a valid file');
+	}
+	const imageUrl = await uploadImage(image);
+
 	const json = createProductSchema.parse({
 		...Object.fromEntries(formData),
 		sellerId: getSetting('user')?._id,
+		image: imageUrl,
 	});
-	const response = await postRequest('product', json, true);
+	const response = await postRequest('product', json);
 	return productModelSchema.parse(response);
 };
