99bc912f06b6fffb29763e6f7c966e615c6ade50
SuperKXT <superkxt@outlook.com>
2022-10-06 01:01:45 +0500

    Overhaul body-types view to add favorite and review

--- src/routes/dashboard/body-types/view/body-types-view.tsx
+++ src/routes/dashboard/body-types/view/body-types-view.tsx
@@ -1,18 +1,58 @@
-import { LoaderFunction, useLoaderData } from 'react-router-dom';
+import {
+	ActionFunction,
+	LoaderFunction,
+	useFetcher,
+	useLoaderData,
+} from 'react-router-dom';
 
 import { BodyType } from 'schemas/body-type';
+import { Favorite } from 'schemas/favorite';
+import { Review } from 'schemas/review';
 import { getBodyTypes } from 'endpoints/body-type';
+import { getReviews } from 'endpoints/review';
+import { createFavorite, getFavorites } from 'endpoints/favorite';
 
 import { humanizeString } from 'helpers/string';
+import { getActionError } from 'helpers/route';
 
 import Page from 'components/Page';
 import Card from 'components/Card';
 
-export const bodyTypesViewLoader: LoaderFunction = getBodyTypes;
+interface LoaderData {
+	bodyTypes: BodyType[],
+	favorites: Favorite[],
+	reviews: Review[],
+}
+
+export const bodyTypesViewLoader: LoaderFunction = async (): Promise<LoaderData> => {
+	const bodyTypes = await getBodyTypes();
+	const favorites = await getFavorites();
+	const reviews = await getReviews();
+	return {
+		bodyTypes,
+		favorites,
+		reviews,
+	};
+};
+
+export const bodyTypesFavoriteAction: ActionFunction = async ({ request }) => {
+	try {
+		const formData = await request.formData();
+		await createFavorite(formData);
+		return null;
+	}
+	catch (error: any) {
+		return getActionError({
+			source: 'body-types-favorite',
+			error,
+		});
+	}
+};
 
 export const BodyTypesView = () => {
 
-	const bodyTypes = useLoaderData() as BodyType[];
+	const { bodyTypes, favorites, reviews } = useLoaderData() as LoaderData;
+	const fetcher = useFetcher();
 
 	return (
 		<Page
@@ -21,22 +61,53 @@ export const BodyTypesView = () => {
 			hasAdd
 			isGridView
 		>
-			{bodyTypes.map(({ _id, name, model }) =>
-				<Card
-					key={_id}
-					title={name}
-					labels={[
-						{
-							title: `Model: ${`${model.year} ${model.name}`}`,
-							color: 'danger',
-						},
-						{
-							title: `Make Type: ${humanizeString(model.makeType.name)}`,
-							color: 'success',
-						},
-					]}
-				/>
-			)}
+			{bodyTypes.map(({ _id, name, model }) => {
+
+				const isFavorite = favorites.some(row => row.bodyTypeId === _id);
+				const isReviewed = favorites.some(row => row.bodyTypeId === _id);
+
+				return (
+					<Card
+						key={_id}
+						title={name}
+						labels={[
+							{
+								title: `Model: ${`${model.year} ${model.name}`}`,
+								color: 'danger',
+							},
+							{
+								title: `Make Type: ${humanizeString(model.makeType.name)}`,
+								color: 'success',
+							},
+						]}
+						actions={[
+							{
+								text: 'Favorite',
+								icon: isFavorite ? 'starFilled' : 'star',
+								color: 'warning',
+								onClick: () => {
+									if (isFavorite) {
+										return fetcher.submit(null, {
+											action: '/favorite',
+											method: 'delete',
+										});
+									}
+									const form = new FormData();
+									form.append('bodyTypeId', _id);
+									fetcher.submit(form, {
+										action: '/favorite',
+										method: 'post',
+									});
+								},
+							},
+							{
+								text: 'Review',
+								icon: 'review',
+							},
+						]}
+					/>
+				);
+			})}
 		</Page>
 	);
 
